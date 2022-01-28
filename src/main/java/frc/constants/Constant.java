28b7f7200821d218d17054425495d7b9aa19fc87
jwoodard <jwoodard@lpslancer.net>
2022-01-27 20:28:34 -0500

    test of slack notifications

--- src/main/java/frc/constants/Constant.java
+++ src/main/java/frc/constants/Constant.java
@@ -3,7 +3,7 @@ package frc.constants;
 //TODO: Add constants
 public class Constant 
 {
-    public static final String shoutout = "AWESOME JOB ELLIOT!!";
+    public static final String shoutout = "AWESOME JOB ELLIOT!!!";
 
     //TODO: Configure shot rpm's
     public static final double LONG_SHOT_SPEED = 0.0;
