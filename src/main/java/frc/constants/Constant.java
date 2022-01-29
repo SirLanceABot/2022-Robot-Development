14279a2b6259d9c8492cfc0423ad3f01fe1c2107
Joel Sawyer <22dsawyer@lpslancer.net>
2022-01-29 09:21:21 -0500

    testing push

--- src/main/java/frc/constants/Constant.java
+++ src/main/java/frc/constants/Constant.java
@@ -5,6 +5,7 @@ public class Constant
 {
     public static final String shoutout = "AWESOME JOB ELLIOT!!!";
     public static final String laptop4 = "Testing push of code";
+    public static final String joel = "Joel is cool";
 
     //TODO: Configure shot rpm's
     public static final double LONG_SHOT_SPEED = 0.0;
