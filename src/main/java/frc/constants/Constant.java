97f020d18160fb8adac2ef3cca3bd6c1cb2a88e7
Team 4237 Laptop 4 <lakeshoreroboticsteam@gmail.com>
2022-01-29 08:59:23 -0500

    laptop 4 test

--- src/main/java/frc/constants/Constant.java
+++ src/main/java/frc/constants/Constant.java
@@ -4,6 +4,7 @@ package frc.constants;
 public class Constant 
 {
     public static final String shoutout = "AWESOME JOB ELLIOT!!!";
+    public static final String laptop4 = "Testing push of code";
 
     //TODO: Configure shot rpm's
     public static final double LONG_SHOT_SPEED = 0.0;
