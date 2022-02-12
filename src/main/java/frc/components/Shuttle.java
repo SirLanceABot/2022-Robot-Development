f49fc8b0c7aeb0d0bc4abe7cf2753f0f6bd05595
Team 4237 Laptop 4 <LakeshoreRoboticsTeam@gmail.com>
2022-02-12 13:05:40 -0500

    Darren - Implemented shuttle FSM

--- src/main/java/frc/components/Shuttle.java
+++ src/main/java/frc/components/Shuttle.java
@@ -38,7 +38,162 @@ public class Shuttle
     // private static CANEncoder firstEncoder = firstStageMotor.getEncoder();
     // private static CANEncoder secondEncoder = secondStageMotor.getEncoder();
 
+    // *** FSM Stuff ***
 
+    // What we want to happen with the motors
+    // FIXME Make variables not static 
+    private static boolean requestStageOneMotorRunning = false;
+    private static boolean requestStageTwoMotorRunning = false;
+
+    /**
+     * List of allowed Shuttle states, each state should have a doAction
+     */
+    public enum State
+    {
+        NO_CARGO_STORED
+        {
+            void doAction()
+            {
+                // Request stage one and two to be turned off
+                requestStageOneMotorRunning = false;
+                requestStageTwoMotorRunning = false;
+            }
+        },
+
+        
+        STORING_CARGO_IN_STAGE_TWO
+        {
+            void doAction()
+            {
+                // Run stage one and two
+                requestStageOneMotorRunning = true;
+                requestStageTwoMotorRunning = true;
+            }
+        },
+
+        CARGO_STORED_IN_STAGE_TWO
+        {
+            void doAction()
+            {
+                // Turn off stage one and two
+                requestStageOneMotorRunning = false;
+                requestStageTwoMotorRunning = false;
+            }
+        },
+        
+        SHOOTING_CARGO_FROM_STAGE_TWO
+        {
+            void doAction()
+            {
+                // Run stage two and turn off stage one
+                requestStageOneMotorRunning = false;
+                requestStageTwoMotorRunning = true;
+            }
+        },
+
+        STORING_CARGO_IN_STAGE_ONE
+        {
+            void doAction()
+            {
+                // Run stage one
+                requestStageOneMotorRunning = true;
+                // requestStageTwoMotorRunning = false;
+            }
+        },
+
+        CARGO_STORED_IN_STAGE_ONE_AND_TWO
+        {
+            void doAction()
+            {
+                // Turn off stage one
+                requestStageOneMotorRunning = false;
+                // requestStageTwoMotorRunning = false;
+            }
+        },
+
+        SHOOTING_CARGO_FROM_STAGE_ONE_AND_TWO
+        {
+            void doAction()
+            {
+                // Run stage one and two
+                requestStageOneMotorRunning = true;
+                requestStageTwoMotorRunning = true;
+            }
+        },
+
+        STORING_CARGO_IN_STAGE_TWO_FROM_ONE
+        {
+            void doAction()
+            {
+                // Run stage one and two
+                requestStageOneMotorRunning = true;
+                requestStageTwoMotorRunning = true;
+            }
+        },
+
+        EJECTING_EXTRA_CARGO
+        {
+            void doAction()
+            {
+                // Eject extra cargo
+                // TODO: Make eject work
+                System.out.println("Eject extra cargo");
+            }
+        };
+    }
+
+    /**
+     * Transitions of FSM
+     * 
+     * each state Transition state is composed of 2 independent variables (current state and event)
+     * and a dependent variable (new state)
+     */
+    public static enum Transition
+    {
+        // Transition table
+        // transition name (current state, event, new state)
+        TRANSITION_1 (State.NO_CARGO_STORED, Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_ACTIVATES, State.STORING_CARGO_IN_STAGE_TWO),
+        TRANSITION_2 (State.STORING_CARGO_IN_STAGE_TWO, Events.event.STAGE_TWO_FULL_SENSOR_ACTIVATES, State.CARGO_STORED_IN_STAGE_TWO),
+        TRANSITION_3 (State.CARGO_STORED_IN_STAGE_TWO, Events.event.SHOOT_IS_CALLED, State.SHOOTING_CARGO_FROM_STAGE_TWO),
+        TRANSITION_4 (State.SHOOTING_CARGO_FROM_STAGE_TWO, Events.event.STAGE_TWO_FULL_SENSOR_DEACTIVATES, State.NO_CARGO_STORED),
+        TRANSITION_5 (State.CARGO_STORED_IN_STAGE_TWO, Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_ACTIVATES, State.STORING_CARGO_IN_STAGE_ONE),
+        TRANSITION_6 (State.STORING_CARGO_IN_STAGE_ONE, Events.event.STAGE_ONE_FULL_SENSOR_ACTIVATES, State.CARGO_STORED_IN_STAGE_ONE_AND_TWO),
+        TRANSITION_7 (State.CARGO_STORED_IN_STAGE_ONE_AND_TWO, Events.event.SHOOT_IS_CALLED, State.SHOOTING_CARGO_FROM_STAGE_ONE_AND_TWO),
+        TRANSITION_8 (State.SHOOTING_CARGO_FROM_STAGE_ONE_AND_TWO, Events.event.STAGE_TWO_FULL_SENSOR_DEACTIVATES, State.STORING_CARGO_IN_STAGE_TWO_FROM_ONE),
+        TRANSITION_9 (State.STORING_CARGO_IN_STAGE_TWO_FROM_ONE, Events.event.STAGE_TWO_FULL_SENSOR_ACTIVATES, State.SHOOTING_CARGO_FROM_STAGE_TWO),
+        TRANSITION_10 (State.CARGO_STORED_IN_STAGE_ONE_AND_TWO, Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_ACTIVATES, State.EJECTING_EXTRA_CARGO),
+        TRANSITION_11 (State.EJECTING_EXTRA_CARGO, Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_DEACTIVATES, State.CARGO_STORED_IN_STAGE_ONE_AND_TWO);
+
+        private final State currentState;
+        private final Events.event event;
+        private final State nextState;
+
+        Transition(State currentState, Events.event event, State nextState)
+        {
+            this.currentState = currentState;
+            this.event = event;
+            this.nextState = nextState;
+        }
+
+        /**
+         * Table lookup to determine new state given teh curret state and event
+         * 
+         * @param currentState
+         * @param event
+         */
+        public static State findNextState(Shuttle.State currentState, Shuttle.Events.event event)
+        {
+            for (Transition transition : Transition.values())
+            {
+                if (transition.currentState == currentState && transition.event == event)
+                {
+                    return transition.nextState;
+                }
+            }
+
+            return currentState;
+        }
+    }
 
 
     // *** CLASS CONSTRUCTOR ***
@@ -87,5 +242,19 @@ public class Shuttle
     public String toString()
     {
         return null;
+    }
+
+    public static class Events
+    {
+        public static enum event
+        {
+            NONE,
+            INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_ACTIVATES,
+            INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_DEACTIVATES,
+            STAGE_ONE_FULL_SENSOR_ACTIVATES,
+            STAGE_TWO_FULL_SENSOR_ACTIVATES,
+            STAGE_TWO_FULL_SENSOR_DEACTIVATES,
+            SHOOT_IS_CALLED;
+        }
     }
 }
--- src/main/java/frc/robot/TeleopMode.java
+++ src/main/java/frc/robot/TeleopMode.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import java.lang.invoke.MethodHandles;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.components.Intake;
 import frc.components.Climber;
 //import frc.components.Drivetrain;
@@ -9,6 +10,7 @@ import frc.components.Shooter;
 import frc.components.Shuttle;
 import frc.components.CargoManager;
 
+import frc.components.Shuttle;
 import frc.controls.DriverController;
 import frc.controls.DriverController.DriverButtonAction;
 import frc.controls.DriverController.DriverAxisAction;
@@ -43,6 +45,21 @@ public class TeleopMode implements ModeTransition
     //private static final Climber CLIMBER = RobotContainer.CLIMBER;
     //private static final Shuttle SHUTTLE = RobotContainer.SHUTTLE;
     //private static final Drivetrain DRIVETRAIN = RobotContainer.DRIVETRAIN;
+
+    private static Shuttle.Events.event[] array =
+    {
+        Shuttle.Events.event.NONE,
+        Shuttle.Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_ACTIVATES,
+        Shuttle.Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_DEACTIVATES,
+        Shuttle.Events.event.STAGE_ONE_FULL_SENSOR_ACTIVATES,
+        Shuttle.Events.event.STAGE_TWO_FULL_SENSOR_ACTIVATES,
+        Shuttle.Events.event.STAGE_TWO_FULL_SENSOR_DEACTIVATES,
+        Shuttle.Events.event.SHOOT_IS_CALLED
+    };
+    
+    private static int i = 0;
+
+    private boolean tracker = false;
 
     // *** CLASS CONSTRUCTOR ***
     public TeleopMode()
@@ -164,5 +181,39 @@ public class TeleopMode implements ModeTransition
     public void exit()
     {
 
+    }
+
+    private void DarrensTestingMode()
+    {
+        
+        Shuttle.Events.event event = Shuttle.Events.event.INTAKE_CARGO_CAN_BE_SHUTTLED_SENSOR_ACTIVATES;
+        Shuttle.State state = Shuttle.State.NO_CARGO_STORED;
+
+        if(tracker != DRIVER_CONTROLLER.getAction(DriverButtonAction.kIntakeToggleOnOff))
+        {
+            event = array[i];
+
+            System.out.println("Pressed");
+            if (i < array.length - 1)
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+        else
+        {
+            // event = Shuttle.Events.event.NONE;
+        }
+
+        state = Shuttle.Transition.findNextState(state, event);
+
+        // System.out.println("State: " + state.toString());
+        SmartDashboard.putString("State", state.toString());
+        SmartDashboard.putString("Event", event.toString());
+
+        tracker = DRIVER_CONTROLLER.getAction(DriverButtonAction.kIntakeToggleOnOff);
     }
 }
