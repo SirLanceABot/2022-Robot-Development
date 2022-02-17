26df8ab54c4f24e54bb7a789adc3146c7e18b651
Owen Doms <25odoms@lpslancer.net>
2022-02-17 17:51:08 -0500

    Uncommented things

--- src/main/java/frc/robot/TeleopMode.java
+++ src/main/java/frc/robot/TeleopMode.java
@@ -3,23 +3,23 @@ package frc.robot;
 import java.lang.invoke.MethodHandles;
 
 import frc.components.Intake;
-// import frc.components.Climber;
-// import frc.drivetrain.Drivetrain;
+import frc.components.Climber;
+import frc.drivetrain.Drivetrain;
 import frc.components.Shooter;
 import frc.components.Shuttle;
-// import frc.components.CargoManager;
+import frc.components.CargoManager;
 
-// import frc.components.Shuttle;
+import frc.components.Shuttle;
 import frc.controls.DriverController;
-// import frc.controls.DriverController.DriverAxisAction;
+import frc.controls.DriverController.DriverAxisAction;
 import frc.controls.DriverController.DriverButtonAction;
-// import frc.controls.DriverController.DriverAxisAction;
+import frc.controls.DriverController.DriverAxisAction;
 // import frc.controls.DriverController.DriverPOVAction;
 // import frc.controls.Logitech.AxisSettings;
 import frc.controls.OperatorController;
 // import frc.controls.Xbox;
 
-// import frc.controls.OperatorController.OperatorAxisAction;
+import frc.controls.OperatorController.OperatorAxisAction;
 import frc.controls.OperatorController.OperatorButtonAction;
 // import frc.controls.Xbox.Button;
 
@@ -41,9 +41,9 @@ public class TeleopMode implements ModeTransition
     private static final Intake INTAKE = RobotContainer.INTAKE;
     private static final OperatorController OPERATOR_CONTROLLER = RobotContainer.OPERATOR_CONTROLLER;
     private static final Shooter SHOOTER = RobotContainer.SHOOTER;
-    // private static final Climber CLIMBER = RobotContainer.CLIMBER;
+    private static final Climber CLIMBER = RobotContainer.CLIMBER;
     private static final Shuttle SHUTTLE = RobotContainer.SHUTTLE;
-    // private static final Drivetrain DRIVETRAIN = RobotContainer.DRIVETRAIN;
+    private static final Drivetrain DRIVETRAIN = RobotContainer.DRIVETRAIN;
 
 
     // *** CLASS CONSTRUCTOR ***
@@ -179,7 +179,7 @@ public class TeleopMode implements ModeTransition
 
         // DRIVETRAIN.rotate(DRIVER_CONTROLLER.getAction(DriverAxisAction.kRotate));
 
-        // DRIVETRAIN.driveBoost(DRIVER_CONTROLLER.getAction(DriverAxisAction.kDriveBoost));
+        // DRIVETRAIN.driveBoost(DRIVER_CONTROLLER.getAction(DriverAxisAction.kDriverBoost));
 
     }
 
