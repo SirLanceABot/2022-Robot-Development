f324b04bdb78db99b141c88175044151d1132213
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2022-02-21 11:29:59 -0500

    OWEN IS DOMINATING

--- src/main/java/frc/vision/Vision.java
+++ src/main/java/frc/vision/Vision.java
@@ -1,8 +1,26 @@
 package frc.vision;
 
 import java.lang.invoke.MethodHandles;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.HashMap;
 
-public class Vision 
+import org.opencv.core.*;
+import org.opencv.core.Core.*;
+// import org.opencv.features2d.FeatureDetector;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.*;
+import org.opencv.objdetect.*;
+
+import frc.vision.*;
+//import frc.vision.GripPipeLine;
+
+public class Vision extends GripPipeLine
 {
     private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
 
@@ -25,6 +43,4 @@ public class Vision
 
 
     // *** CLASS & INSTANCE METHODS ***
-
-    
 }
