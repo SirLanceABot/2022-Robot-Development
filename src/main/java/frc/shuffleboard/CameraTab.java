6325590e7ba02798a407e8c0cf79439deb8120e1
Team 4237 Laptop 1 <LakeshoreRoboticsTeam@gmail.com>
2022-03-30 20:08:42 -0400

    DF - Started making timer widget

--- src/main/java/frc/shuffleboard/CameraTab.java
+++ src/main/java/frc/shuffleboard/CameraTab.java
@@ -2,6 +2,8 @@ package frc.shuffleboard;
 
 import java.lang.invoke.MethodHandles;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+
 public class CameraTab 
 {
     private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
@@ -15,16 +17,37 @@ public class CameraTab
 
     // *** CLASS & INSTANCE VARIABLES ***
 
+    // Create text output boxes
+    private NetworkTableEntry timeRemaining;
+    private NetworkTableEntry compressorState;
 
+    private String timeRemainingNoDataMessage = "No data";
 
     // *** CLASS CONSTRUCTOR ***
     public CameraTab()
     {
+        System.out.println(fullClassName + " : Constructor Started");
 
+        // timeRemaining = createTimeRemainingBox();
+        // timeRemaining.setString("No Errors");
+
+        System.out.println(fullClassName + ": Constructor Finished");
     }
 
 
     // *** CLASS & INSTANCE METHODS ***
+    // private NetworkTableEntry createTimeRemainingBox()
+    // {
+    //     return CameraTab.add("Error Messages", timeRemainingNoDataMessage)
+    //          .withWidget(BuiltInWidgets.kTextView)
+    //          .withPosition(1, 10)
+    //          .withSize(26, 2)
+    //          .getEntry();
+    // }
 
+    public void updateTimeRemaining()
+    {
+        timeRemaining.setString(timeRemainingNoDataMessage);
+    }
     
 }
