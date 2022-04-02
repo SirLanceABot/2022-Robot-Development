c7d70b5f3936e0b2d480f6346d18179111637ba3
Team 4237 Laptop 5 <LakeshoreRoboticsTeam@gmail.com>
2022-04-02 12:56:21 -0400

    OD vision

--- src/main/java/frc/shuffleboard/CameraTab.java
+++ src/main/java/frc/shuffleboard/CameraTab.java
@@ -2,6 +2,21 @@ package frc.shuffleboard;
 
 import java.lang.invoke.MethodHandles;
 
+import java.lang.invoke.MethodHandles;
+
+import java.util.HashMap;
+import java.util.Map;
+import frc.components.Intake;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.components.Shooter;
+import edu.wpi.first.util.sendable.SendableRegistry;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
 
 public class CameraTab 
@@ -20,6 +35,7 @@ public class CameraTab
     // Create text output boxes
     private NetworkTableEntry timeRemaining;
     private NetworkTableEntry compressorState;
+    static ShuffleboardTab cameraTab;
 
     private String timeRemainingNoDataMessage = "No data";
 
@@ -30,6 +46,15 @@ public class CameraTab
 
         // timeRemaining = createTimeRemainingBox();
         // timeRemaining.setString("No Errors");
+        cameraTab = Shuffleboard.getTab("Camera");
+        // CameraWidget cw = new CameraWidget(cameraTab);
+        // cw.name("Intake");
+        // cw.setLocation(0, 0, 13, 14);
+        // cw.setLocation(0, 0, 16, 19); // For big screen
+        // cw.setProperties(false, "white", false, "NONE");
+        // cw.createCameraShuffleboardWidget(intakeCameraServer.getSource());
+        
+        Shuffleboard.update();
 
         System.out.println(fullClassName + ": Constructor Finished");
     }
@@ -50,4 +75,28 @@ public class CameraTab
         timeRemaining.setString(timeRemainingNoDataMessage);
     }
     
+    // public void cameraStreamMode()
+    // {
+    //     if(cameraMode != null)
+    //         cameraMode = cameraMode.getDouble(2.0);
+    // }
+
+    // private void createCameraModeBox()
+    // {
+    //     //create and name the Box
+    //     SendableRegistry.add(cameraModeBox, "Camera Mode");
+    //     SendableRegistry.setName(cameraModeBox, "Camera Mode");
+        
+    //     //add options to  Box
+    //     cameraModeBox.setDefaultOption("0", CameraTabData.CameraMode.k0);
+    //     cameraModeBox.addOption("1", CameraTabData.CameraMode.k1);
+    //     cameraModeBox.addOption("2", CameraTabData.CameraMode.k1);
+
+    //     //put the widget on the shuffleboard
+    //     CameraTab.add(cameraModeBox)
+    //         .withWidget(BuiltInWidgets.kSplitButtonChooser)
+    //         .withPosition(0, 0)
+    //         .withSize(8, 2);
+    // }
+
 }
